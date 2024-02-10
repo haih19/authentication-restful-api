a23da3e812f7565e4784f75846b38d928e101cd6
hai hoang <65638605+haih19@users.noreply.github.com>
2024-02-11 04:00:01 +0700

    feat: authentication controller

---
+++ src/helpers/index.ts
@@ -0,0 +1,15 @@
+import crypto from "crypto";
+import { config } from "dotenv";
+
+config();
+
+const secretKey = process.env.SECRET_KEY;
+
+export const random = () => crypto.randomBytes(128).toString("base64");
+
+export const authentication = (salt: string, password: string) => {
+  return crypto
+    .createHmac("sha256", [salt, password].join("/"))
+    .update(secretKey)
+    .digest("hex");
+};
